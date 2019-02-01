7f9d90c715232a091cfd3e82f31b4f6c907bab71
Max <maximumsomething@gmail.com>
2019-02-01 12:37:47 -0500

    Receives and uses vision data

---
+++ src/main/cpp/subsystems/VisionReceiver.cpp
@@ -0,0 +1,105 @@
+#include "subsystems/VisionReceiver.h"
+#include "Robot.h"
+#include <iostream>
+#include <streambuf>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+constexpr char VISION_PORT[] = "8081";
+
+VisionReceiver::VisionReceiver() : Subsystem("VisionReceiver"),
+visionDataStream(&visionDataStreamBuf) {
+	setupSocket();
+}
+
+void VisionReceiver::setupSocket() {
+	
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;   /* For wildcard IP address */
+
+	struct addrinfo *result;
+
+	int error = getaddrinfo(nullptr, VISION_PORT, &hints, &result);
+	if (error != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+	}
+
+	for (struct addrinfo *rp = result; rp != NULL; rp = rp->ai_next) {
+               sockfd = socket(rp->ai_family, rp->ai_socktype,
+                       rp->ai_protocol);
+        if (sockfd != -1) {
+
+			if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) break;
+			else {
+				perror("failed to bind to vision socket");
+				close(sockfd);
+				sockfd = -1;
+			}
+		}
+	}
+	freeaddrinfo(result);
+
+	if (sockfd == -1) {
+		std::cout << "could not connect to vision socket" << std::endl;
+	}
+}
+
+void VisionReceiver::Periodic() {
+
+	while (true) {
+	char buf[66537];
+
+		ssize_t recieveSize = recvfrom(sockfd, buf, sizeof(buf) - 1, 
+		MSG_DONTWAIT, nullptr, nullptr);
+
+		if (recieveSize > 0) {
+
+			buf[recieveSize] = '\0';
+			visionDataStream << buf;
+		}
+		else break;
+	}
+	std::string line;
+	while (std::getline(visionDataStream, line)) {
+		if (line[0] == '#') {
+
+			int isPort, num;
+			TargetData data;
+			sscanf(line.c_str(), "#%d: isPort=%d distance=%lf tapeAngle=%lf robotAngle=%lf",
+			&num, &isPort, &data.distance, &data.tapeAngle, &data.robotAngle);
+			readTapes.push_back(data);
+		}
+		else if (line[0] == '@') {
+			if (readTapes.size() > 0) {
+
+				sscanf(line.c_str(), "@%d", &latency);
+
+				//TODO: correct for latency
+				AutoDrive::RobotPosition robPos = Robot::autoDrive.currentPosition;
+				targetLocs.clear();
+				for (auto i : readTapes) {
+
+					TargetLoc target;
+					double wholeAngle = robPos.angle/180*M_PI + i.robotAngle;
+
+					target.loc.x = robPos.loc.x + i.distance*sin(wholeAngle);
+					target.loc.y = robPos.loc.y + i.distance*cos(wholeAngle);
+
+					target.angle = i.tapeAngle;
+
+					targetLocs.push_back(target);
+				}
+				newData = true;
+			}
+		}
+		else {
+			std::cout << "vision data parse error" << std::endl;
+		}
+	}
+}
--- src/main/include/subsystems/AutoDrive.h
+++ src/main/include/subsystems/AutoDrive.h
@@ -12,21 +12,27 @@ public:
 	// left is negative, right positive. Forward is positive, behind the wall would be negative.
 	// clockwise positive, counterclockwise negative.
 	struct RobotPosition {
-		Point point;
+		Point loc;
 		double angle; 
 		double encoderDistance; // revolutions
 	};
 	RobotPosition currentPosition;
 
 	struct Target {
-		Point point;
+		Point loc;
 		bool isAngled, slowDown;
 		double angle;
 	};
 	Target target;
 
+	bool passedTarget(Point from);
+
+	// Call this from the command that is using AutoDrive.
+	// Wpilib's scheduler calls subsystems before commands, 
+	// so we can't just use Subsystem::Periodic().
+	void updatePower();
+
 	AutoDrive();
-	void Periodic() override;
 
 private:
 	std::chrono::steady_clock clock;
