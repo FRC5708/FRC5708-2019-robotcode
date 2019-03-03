e960f6a44ad3c9af05202366b49658b27ec9c6c7
Andrew Plotner <plotner@Todd-Plotners-Home-MacBook-Air.local>
2019-03-03 10:25:16 -0500

    Added autonomous shooting for a duration of time.

---
+++ src/main/include/commands/McShootieAuto.h
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+#ifndef SRC_COMMANDS_BALLSHOOT_H_
+#define SRC_COMMANDS_BALLSHOOT_H_
+#include <frc/commands/Command.h>
+#include "Robot.h"
+#include "McShootieTube.h"
+#include <chrono>
+
+class BallShoot : public frc::Command {
+public:
+	BallShoot(){};
+	
+	void Initialize() override {
+		startTime = std::chrono::steady_clock::now();
+	}
+	void Execute() override {
+		Robot::manipulator.Shoot();
+	}
+	bool IsFinished() override {
+		if (std::chrono::steady_clock::now() > startTime + std::chrono::milliseconds(2000)) {
+			Robot::manipulator.Stop();
+			return true;
+		}
+		return false;
+	}
+		private:
+	std::chrono::steady_clock::time_point startTime;
+};
+#endif 
