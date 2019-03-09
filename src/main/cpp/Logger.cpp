a31da09343710a74bce7c2aa48d6bad1cea2fa08
Andrew Plotner <plotner@Todd-Plotners-Home-MacBook-Air.local>
2019-03-09 09:09:58 -0500

    Fixed logger

--- src/main/cpp/Logger.cpp
+++ src/main/cpp/Logger.cpp
@@ -2,18 +2,21 @@
 #include <fstream>
 #include <time.h>
 #include <string>
+#include <sstream>
 
 using namespace std;
 class Logger{
     string name;
     ofstream output_file;
-    Logger(string name="NONE",string output_file=""){
+    Logger(string name="NONE",string output_filepath=""){
         this->name=name;
         time_t rawtime; time (&rawtime); struct tm *tm_struct = localtime(&rawtime);
-        if(output_file==""){
-            output_file = ("~/Log_" + name + ':' + (tm_struct->tm_hour) + ':' + (tm_struct->tm_min) + ':' + (tm_struct->tm_sec));
+        if(output_filepath==""){
+            stringstream output;
+            output <<  "~/Log_" << name <<  ':' << (tm_struct->tm_hour) << ':' << (tm_struct->tm_min) << ':' << (tm_struct->tm_sec) << ".txt";
+            output_filepath=output.str();
         }
-        this->output_file.open(output_file,ofstream::out | ofstream::trunc);
+        this->output_file.open(output_filepath,ofstream::out | ofstream::trunc);
     }
     void log(char* message){
         time_t rawtime; time (&rawtime); struct tm *tm_struct = localtime(&rawtime);
